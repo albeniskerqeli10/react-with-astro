e3b2eb39bd83f9d082b6f542549a04178c205a73
Albenis Kërqeli <albeniskerqeli5@gmail.com>
2022-09-09 01:04:24 +0000

    s

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -8,7 +8,7 @@ imageUrl:string;
 const Blog: FunctionComponent<BlogProps> = ({id,title,imageUrl,}) => {
 return (
     <article key={id} className="w-[500px] rounded-sm shadow-lg [vertical-align:top] bg-white flex items-center justify-center flex-col">
-                <img loading="lazy" className="w-full max-w-full h-[250px] object-cover" src={imageUrl} alt="pic"/>
+                <img className="w-full max-w-full h-[250px] object-cover" src={imageUrl} alt="pic"/>
 
         <h1 className='text-xl font-bold py-3 px-3'>{title}
         </h1>
